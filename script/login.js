b1a14d03f9860cffc135c3917de62aa96d26959e
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-01 03:21:29 +0600

    Login Page Complete

---
+++ script/login.js
@@ -0,0 +1,15 @@
+document.getElementById("login_btn").addEventListener("click", function () {
+    const mobileNumber = document.getElementById("mobile_number");
+    const idNumber = mobileNumber.value;
+
+    const pin = document.getElementById("pin")
+    const pinNumber = pin.value;
+
+    if (idNumber == "01876566181" && pinNumber == "6181") {
+        alert("login Success")
+    } else {
+        alert("Wrong Input")
+    }
+
+    return
+})
