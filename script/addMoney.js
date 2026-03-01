f6c746849f2181d6f3fbce91c39544afb09e5fa2
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-02 04:00:41 +0600

    Add Money Section Complete

---
+++ script/addMoney.js
@@ -0,0 +1,29 @@
+document.getElementById("add_money_btn").addEventListener("click", function () {
+    const bankAccount = getValueFromInput("add_money_bank");
+    if (bankAccount == "Select A Bank") {
+        alert("Please Select A Bank");
+        return;
+    }
+
+    const bankAccountNumber = getValueFromInput("bank_account_number");
+    if (bankAccountNumber.length != 11) {
+        alert("Account Number is Invalid");
+        return;
+    }
+
+    const addMoneyAmount = getValueFromInput("add_money_amount");
+
+    const currentAmount = getBalance();
+
+    const newBalance = Number(addMoneyAmount) + currentAmount;
+
+    const pin = getValueFromInput("add_money_pin");
+    if (pin == 6181) {
+        alert("Add Money Successfull");
+        setBalance(newBalance)
+        return;
+    } else {
+        alert("Your Pin is Invalid");
+        return;
+    }
+})
